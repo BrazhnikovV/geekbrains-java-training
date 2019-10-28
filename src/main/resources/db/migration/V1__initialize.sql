40594adf9942f4228204bab81e3c5a8937e11661
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-10-28 19:07:59 +0300

    Расширил гитигнор.

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -1,46 +1,46 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 -- noinspection SqlDialectInspectionForFile
-
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id                    bigserial,
-  username              VARCHAR(50) NOT NULL UNIQUE,
-  password              VARCHAR(80),
-  email                 VARCHAR(50) UNIQUE,
-  phone                 VARCHAR(15),
-  first_name            VARCHAR(50),
-  last_name             VARCHAR(50),
-  PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS roles;
-CREATE TABLE roles (
-  id                    serial,
-  name                  VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS users_roles;
-CREATE TABLE users_roles (
-  user_id               INT NOT NULL,
-  role_id               INT NOT NULL,
-  PRIMARY KEY (user_id, role_id),
-  FOREIGN KEY (user_id)
-  REFERENCES users (id),
-  FOREIGN KEY (role_id)
-  REFERENCES roles (id)
-);
-
-INSERT INTO roles (name)
-VALUES
-('ROLE_USER'), ('ROLE_ADMIN');
-
-INSERT INTO users (username, password, email, phone, first_name, last_name)
-VALUES
-('admin','$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'admin@gmail.com', '+79380000000', 'admin', 'adminson');
-
-INSERT INTO users_roles (user_id, role_id)
-VALUES
-(1, 1),
-(1, 2);
+#
+# DROP TABLE IF EXISTS users;
+# CREATE TABLE users (
+#   id                    bigserial,
+#   username              VARCHAR(50) NOT NULL UNIQUE,
+#   password              VARCHAR(80),
+#   email                 VARCHAR(50) UNIQUE,
+#   phone                 VARCHAR(15),
+#   first_name            VARCHAR(50),
+#   last_name             VARCHAR(50),
+#   PRIMARY KEY (id)
+# );
+#
+# DROP TABLE IF EXISTS roles;
+# CREATE TABLE roles (
+#   id                    serial,
+#   name                  VARCHAR(50) NOT NULL,
+#   PRIMARY KEY (id)
+# );
+#
+# DROP TABLE IF EXISTS users_roles;
+# CREATE TABLE users_roles (
+#   user_id               INT NOT NULL,
+#   role_id               INT NOT NULL,
+#   PRIMARY KEY (user_id, role_id),
+#   FOREIGN KEY (user_id)
+#   REFERENCES users (id),
+#   FOREIGN KEY (role_id)
+#   REFERENCES roles (id)
+# );
+#
+# INSERT INTO roles (name)
+# VALUES
+# ('ROLE_USER'), ('ROLE_ADMIN');
+#
+# INSERT INTO users (username, password, email, phone, first_name, last_name)
+# VALUES
+# ('admin','$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'admin@gmail.com', '+79380000000', 'admin', 'adminson');
+#
+# INSERT INTO users_roles (user_id, role_id)
+# VALUES
+# (1, 1),
+# (1, 2);
